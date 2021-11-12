c9ce0fb3477ef8784ec098c930e00bad627a8864
aali195 <amd5910@gmail.com>
2021-11-12 04:59:42 +0000

    feat: improve choose command to allow for spaced options

--- src/main/kotlin/com/github/ball/ballbot/commands/ChooseCommand.kt
+++ src/main/kotlin/com/github/ball/ballbot/commands/ChooseCommand.kt
@@ -5,8 +5,11 @@ object ChooseCommand : Command() {
     override val command: String = "choose"
 
     override fun execute(context: CommandContext): Unit = with(context) {
-        commandArgs
+        message.contentDisplay
+            .removePrefix("$prefix$command")
+            .trimIndent()
             .takeIf { it.isNotEmpty() }
+            ?.split(DELIMITER)
             ?.run { message.reply(random()).queue() }
             ?: run { message.reply("its: $usage").queue() }
     }
@@ -16,7 +19,9 @@ object ChooseCommand : Command() {
     """.trimIndent()
 
     override val usage: String = """
-        `[prefix]choose [items with spaces between]`
+        `[prefix]choose [items with ',' between (spaces allowed)]`
     """.trimIndent()
 
 }
+
+private const val DELIMITER = ","
